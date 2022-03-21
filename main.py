31314266c8a9575ae3fbf466816bb189120c84e1
nguyennicola <nguyennicolasviet@gmail.com>
2022-03-21 19:53:54 +0100

    'comments'

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scipy.stats import norm
 import tqdm 
 
 def fi(mu_i, i, mu_minus_i, sigma, gamma, X, Y, lamb):
+    """ minimize this function wrt mu """
     f = mu_i * (X.T @ X)[i, np.arange(X.shape[1])!=i] @ (gamma[np.arange(len(gamma))!=i] * mu_minus_i)
     f += 0.5*(X.T @ X)[i, i] * mu_i**2
     f -= (Y.T @ X)[i] * mu_i
@@ -15,7 +16,7 @@ def fi(mu_i, i, mu_minus_i, sigma, gamma, X, Y, lamb):
 
 
 def gi(sigma_i, i, mu, X, lamb):
-
+    """ minimize this function wrt sigma """
     g = 0.5 * (X.T @ X)[i, i] * sigma_i**2
     g += lamb * mu[i] * sigma_i * np.sqrt(2/np.pi) * np.exp(-mu[i]**2/(2*sigma_i**2))
     g += lamb * mu[i] * (1 - norm.cdf(mu[i]/sigma_i))
@@ -23,6 +24,7 @@ def gi(sigma_i, i, mu, X, lamb):
     return g
 
 def Gamma_function(i, mu, sigma, gamma, X, Y, a0, b0, lamb):
+    """ closed-form formula to update gamma """
     Gamma = np.log(a0/b0)
     Gamma += np.log(np.sqrt(np.pi/2)*sigma[i]*lamb)
     Gamma -= mu[i] * (X.T @ X)[i, np.arange(X.shape[1])!=i] @ (gamma[np.arange(len(gamma))!=i] * mu[np.arange(len(mu))!=i])
@@ -35,6 +37,7 @@ def Gamma_function(i, mu, sigma, gamma, X, Y, a0, b0, lamb):
 
 
 def H(p, ent):
+    """ Binary entropy function """
     for j in range(len(p)):
         if p[j] < 1 - 1e-10 and p[j] > 1e-10:
             ent[j] = -p[j]*np.log(p[j]) - (1-p[j])*np.log(1-p[j])
@@ -42,6 +45,7 @@ def H(p, ent):
 
 
 def mu_0(X, Y):
+    """ Initialization for mu """
     _, p = X.shape
     return np.linalg.inv(X.T @ X + np.eye(p)) @ X.T @ Y
 
@@ -57,9 +61,19 @@ def inv_logit(p):
 
 
 def variational_laplace(X, Y, sigma, gamma, mu, a0, b0, lamb, order='priority', eps=1e-5, max_it=1):
+    """
+    Main algorithm for Laplace prior slab 
+
+        sigma, gamma, mu : initialized variational parameters
+        a0, b0 : beta distribution hyperparameters
+        lamb : regularization parameter
+        order : 'priority' or 'lexicographical' or 'random'
+
+    """
     deltaH = 10
     p = len(mu)
     it = 0
+
     ## choose order
     if order=='priority':
         a = np.argsort(np.abs(mu))[::-1]
@@ -68,10 +82,12 @@ def variational_laplace(X, Y, sigma, gamma, mu, a0, b0, lamb, order='priority', 
     
     pbar = tqdm.tqdm(total=max_it)
     ent = H(np.random.random(p), np.random.random(p))
+
     while it < max_it and deltaH >= eps:
         pbar.update(1)
         if order=='random':
             a = np.random.choice(p, size=p, replace=False)
+
         for j in range(p):
             i = a[j]
             ## update mu_i
@@ -87,15 +103,20 @@ def variational_laplace(X, Y, sigma, gamma, mu, a0, b0, lamb, order='priority', 
             ## update gamma
             Gamma = Gamma_function(i, mu, sigma, gamma, X, Y, a0, b0, lamb)
             gamma[i] = inv_logit(Gamma)
+
         it += 1
         ent_old = np.copy(ent)
         ent = H(gamma, ent)
         deltaH = np.max(np.abs(ent) - np.abs(ent_old))
+
     pbar.close( )
     return mu, sigma, gamma
 
 
 def variational_gaussian(X, Y, sigma, gamma, mu, a0, b0, lamb, eps=1e-5, max_it=1):
+    """
+    Same algorithm for Gaussian prior slab (closed-form formula)
+    """
     deltaH = 10
     p = len(mu)
     it = 0
